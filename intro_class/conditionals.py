95cbcf85479b098e174deaddfc6a8d5592da594f
zahras09 <victoriayiyau@yahoo.com>
2016-04-21 21:15:05 -0700

    Conditionals calss

---
+++ intro_class/conditionals.py
@@ -0,0 +1,48 @@
+# theirname="Jamie"
+
+# if myname>theirname:
+#     print "My name is greater!"
+# elif myname<theirname:
+#     print "Their name is greater!"
+# else:
+#     print "Our names are equal!"
+
+# today_day = 21
+# if today_day >= 15:
+# 	print "Oh, we're halfway there!"
+# else:
+# 	print "The month is still young."
+
+# today = "Thursday"
+# if today == "Monday":
+#     print "Yay class day!"
+# elif today == "Tuesday":
+# 	print "Sigh, it's only Tuesday"
+# elif today == "Wednesday":
+# 	print "Humpday!"
+# elif today == "Thursday":
+# 	print "#tbt"
+# elif today == "Friday":
+# 	print "TGIF!"	
+# else:
+# 	print "Yeah, it's the weekend!"
+
+# age = int(raw_input("What is your age?"))
+
+# # if age >= 18:
+# # 	print "Yay! I can vote!"
+# # else:
+# # 	print "Aww, I cannot vote."
+
+# # if age >= 18 and age >= 21:
+# # 	print "Yay! I can vote and go to the bar!"
+# # else:
+# # 	print "Aww, I cannot vote."
+
+# if age >= 21:
+# 	print "I can go to the bar!"
+# elif age >=18 and age < 21:
+# 	print "I can vote but I cannot go a bar."
+# else:
+# 	print "I cannot vote or go to a bar."
+
